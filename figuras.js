f54af9b0b31255632caa78a8d2f8f0378ded4124
Ginna Mora <higinnamora@gmail.com>
2022-04-20 23:44:12 -0500

    Practico-Javascript: class 1

--- figuras.js
+++ figuras.js
@@ -1,1 +1,62 @@
-console.log("Hello World");
+// Código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm^2");
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log(
+  "Los lados del triángulo miden: " +
+    ladoTriangulo1 +
+    "cm, " +
+    ladoTriangulo2 +
+    "cm, " +
+    baseTriangulo +
+    "cm"
+);
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triángulo es de: " + alturaTriangulo + "cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triángulo es: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triángulo es: " + areaTriangulo + "cm^2");
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Circulos");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del circulo mide: " + radioCirculo + "cm");
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo mide: " + diametroCirculo + "cm");
+
+// PI
+const PI = Math.PI;
+console.log("El número PI es" + PI);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perimetro del circulo mide: " + perimetroCirculo + "cm");
+
+//Area
+const areaCirculo = radioCirculo * radioCirculo * PI;
+console.log("El área del circulo mide: " + areaCirculo + "cm");
+
+console.groupEnd();
